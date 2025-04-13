dcb8835352cb55b7831964cb02606924c6b83192
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2025-04-13 16:14:21 +0700

    update

--- suites/02. Regression/RegressionTest.suite.js
+++ suites/02. Regression/RegressionTest.suite.js
@@ -1,3 +1,3 @@
 module.exports = [
-    'TC_Additional_Details_01_Verify_that_the_Timestamps_display_Information_Received_as_a_most_outstanding_feature.spec.js'
+    'ui/ShipmentDetails/TC_Additional_Details_01_Verify_that_the_Timestamps_display_Information_Received_as_a_most_outstanding_feature.spec.js.spec.js'
 ];
