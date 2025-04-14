74bde7ee5d5346dac4ab03a1684f0ba6e2003a35
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2025-04-14 19:27:44 +0700

    update

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
-	workers: process.env.CI ? process.env.CI  : undefined,
+	workers: process.env.CI ? Number(process.env.CI) : 1,
 	reporter: [["html"], ["list"], ["json", { outputFile: "playwright-report/test-results.json" }]],
 	use: {
 		baseURL: process.env.BASE_URL || "http://localhost:9081",
