08afa2b3adfb49aa19550779ad23af9196e9a86a
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2025-04-15 22:36:05 +0700

    update

--- tcs/ui/ShipmentDetails/TC_Intentional_Failed.spec.js
+++ tcs/ui/ShipmentDetails/TC_Intentional_Failed.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test.describe('TC_Intentional_Failed', () => {
-    test('This test is intentionally failing to demonstrate failure handling', async ({ page }) => {
+    test('TC_Intentional_Failed', async ({ page }) => {
         // This test is intentionally failing to demonstrate:
 
         // Intentionally fail the test with a clear message
